59489e484a9052c2ac1d35154f002e1e9bbcb3a6
mahdi msr <mahdi.msr4@gmail.com>
2026-08-04 11:12:38 +0330

    rabbit: handle env variable

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"quantum-signal/scheduler"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error loading .env file : %s", err)
+	}
+
 	fmt.Println("📶 Quantum signal started...")
 
 	scheduler.Start(5)
