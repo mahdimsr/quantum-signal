03eb4f2ad7b5ce8331b65465454577ca1b53bd16
mahdi msr <mahdi.msr4@gmail.com>
2026-08-05 17:22:08 +0330

    dev: prepare sending signal structure

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -3,6 +3,9 @@ package scheduler
 import (
 	"fmt"
 	"log"
+	"quantum-signal/candles"
+	"quantum-signal/indicator"
+	"quantum-signal/strategy"
 	"time"
 )
 
@@ -18,11 +21,32 @@ func Start(minutes int) {
 		nextRun := getNextRunMark(now, minutes)
 		waitDuration := time.Until(nextRun)
 
+		/*rabbitService, err := rabbitmq.NewRabbitMqService()
+		if err != nil {
+			log.Fatalf("Failed to create rabbitmq service: %s", err)
+		}*/
+
 		log.Printf("⌚ Next execution at: %s (waiting %v)", nextRun.Format(time.RFC3339), waitDuration)
 
 		time.Sleep(waitDuration)
 
 		log.Printf("🔄 Executing trading task at: %s", time.Now().UTC().Format(time.RFC3339))
+
+		candleService := candles.NewCandleService(candles.EXCHANGE, "BTCUSDT", "15m", 100)
+		candlesList := candleService.GetCandles()
+
+		utbotValues := indicator.NewUTBot(3, 5, false)
+		utbotStrategy := strategy.NewUTBotStrategy(utbotValues)
+		signals := utbotStrategy.GenerateSignals(candlesList)
+
+		lastSignal := signals[len(signals)-1]
+
+		signalTime := time.UnixMilli(lastSignal.Timestamp).UTC()
+
+		if IsWithinDuration(now, signalTime, 15) {
+
+			// TODO: send signal to rabbit
+		}
 
 	}
 
@@ -45,7 +69,7 @@ func getNextRunMark(now time.Time, cycleMinutes int) time.Time {
 		time.UTC,
 	)
 
-	if nextMinutes >= 60 {
+	/*if nextMinutes >= 60 {
 		nextRun = nextRun.Add(time.Hour)
 		nextRun = time.Date(
 			nextRun.Year(),
@@ -57,7 +81,11 @@ func getNextRunMark(now time.Time, cycleMinutes int) time.Time {
 			0,
 			time.UTC,
 		)
-	}
+	}*/
 
 	return nextRun
+}
+
+func IsWithinDuration(t1, t2 time.Time, threshold time.Duration) bool {
+	return t1.Sub(t2).Abs() < threshold
 }
